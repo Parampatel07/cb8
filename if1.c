4059f8c92808376826ba17029db8b3ac7f4e3b54
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-07-26 15:20:00 +0530

    first push

---
+++ if1.c
@@ -0,0 +1,14 @@
+//write a programe to print number is 10 if it is 
+#include<stdio.h>
+void main()
+{
+    int number;
+    printf("Enter your number");
+    scanf("%d",&number);
+    if(number==10)
+    {
+        printf("this number is 10\n");
+    }
+    
+    printf("Goodbye..");
+}
