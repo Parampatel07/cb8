16caf78ebeb120eb3f6daf28243e2b3e295f7081
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-09-03 15:38:00 +0530

    alphabet

---
+++ alphabet.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+void main()
+{
+    int count = 0;
+    // while (count < 6)
+    // {
+    //     printf("*");
+    //     count++;
+    // }
+    // printf("\n");
+    count = 0;
+    while (count < 6)
+    {
+        printf("*");
+        count++;
+    }
+    count = 0;
+    while (count < 3)
+    {
+        printf("\n");
+        printf("**");
+        printf("  ");
+        printf("**");
+        count++;
+    }
+    count = 0;
+    printf("\n");
+    while (count < 5)
+    {
+        printf("*");
+        count++;
+    }
+    printf("\n");
+    count = 0;
+    while (count < 4)
+    {
+        printf("*");
+        count++;
+    }
+    printf("\n");
+    count = 0;
+    while (count < 3)
+    {
+        printf("*");
+        count++;
+    }
+    printf("\n");
+    count = 0;
+    while (count < 4)
+    {
+        printf("*");
+        count++;
+    }
+    printf("\n");
+    printf("**");
+    printf(" ");
+    printf("**");
+    printf("\n");
+    printf("**");
+    printf("  ");
+    printf("**");
+}
--- function7.c
+++ function7.c
@@ -3,13 +3,10 @@
 void main()
 {
     int num1,num2;
-
     printf("Enter value of num1 ");
     scanf("%d",&num1);
-    
     printf("Enter value of num2 ");
     scanf("%d",&num2);
-
     printf(" 1 for addtion ");
     printf(" 2 for subtraction");
     printf(" 3 for multipilcation");
@@ -17,6 +14,5 @@ void main()
     printf(" 5 for modlus");
     printf(" 6 for maximum ");
     printf(" 7 for minimum ");
-    printf(" 0 for  exit ");
-
+    printf(" 0 for exit ");
 }
